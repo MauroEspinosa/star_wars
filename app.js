e8995fca20d9853f2e152e425f2c7ee7231e6f7b
MauroEspinosa <maauurooxd@hotmail.com>
2016-12-31 15:05:29 -0600

    0.1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 ﻿var express=require("express");
 var app=express();
-var path = require('path');
-var funciones=require(path.join(process.cwd()+"/routes/index"));
+var funciones=require("./routes/index");
+var path=require('path');
 
-app.set('views', path.join(process.cwd() +'/views'));
-app.use(express.static(path.join(process.cwd() + '/public')));
+app.set('views', path.join(process.cwd()+'/views'));
+app.use(express.static(path.join(process.cwd()+'/public')));
 app.set("view engine", "ejs");
 
 app.get("/", funciones.home);
